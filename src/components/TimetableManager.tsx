dcac840177b497c78a4f7059d551474166c2fa07
SHUBHAM <80262881+theshubhamgundu@users.noreply.github.com>
2025-07-24 23:13:15 +0530

    Update TimetableManager.tsx

--- src/components/TimetableManager.tsx
+++ src/components/TimetableManager.tsx
@@ -8,7 +8,7 @@ import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Edit, Plus, Trash2 } from 'lucide-react';
 import { useToast } from '@/hooks/use-toast';
-import { supabase } from '@/integrations/supabase/client';
+import { supabaseOld } from '@/integrations/supabase/supabaseOld';
 import AddSlotModal from './AddSlotModal';
 
 const TimetableManager = () => {
