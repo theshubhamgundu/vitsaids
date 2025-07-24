6f276e6a15404ee9bed64ae714edc036e60acc16
SHUBHAM <80262881+theshubhamgundu@users.noreply.github.com>
2025-07-24 23:03:59 +0530

    Update StudentDashboard.tsx

--- src/pages/StudentDashboard.tsx
+++ src/pages/StudentDashboard.tsx
@@ -1,7 +1,7 @@
 // StudentDashboard.tsx
 import React, { useEffect, useState, useRef, useCallback } from 'react'; // Added useCallback
 import { useAuth } from '@/contexts/AuthContext';
-import { supabase } from '@/integrations/supabase/client';
+import { supabase } from '@/integrations/supabase/supabaseNew';';
 import { useLocation } from 'wouter';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
